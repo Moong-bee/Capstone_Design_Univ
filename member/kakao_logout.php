361ae8bbf888b9e9516c399f6bc98375c2d48c9e
P-bin <qkrgksqls9@gmail.com>
2018-05-28 20:37:06 +0900

    Revert "파일수정"
    This reverts commit fc751d52c266e1d21592d8c91df9f9a61a9fc440.

--- member/kakao_logout.php
+++ member/kakao_logout.php
@@ -1,5 +1,5 @@
 <?php
-$TOKEN_API_URL = "https://kapi.kakao.com/v1/user/logout";
+$TOKEN_API_URL = "https://kapi.kakao.com/v1/user/unlink";
 
 $header = array('Authorization: Bearer ' . $_SESSION['access_token']);
 $opts = array(
@@ -21,5 +21,5 @@ $return_data = json_decode($return_data, true);
 
 unset($_SESSION['userid']);
 
-echo("<script>location.href = 'http://localhost:8080/Capstone-PHP/index.php'</script>");
+echo("<script>location.href = 'http://localhost:8080/Capstone-PHP/index2.php'</script>");
 ?>
